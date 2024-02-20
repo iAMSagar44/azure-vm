012401b1a9c58839c40abde7ae1ca585fbe29cb4
Sagar <sagar.jobs@outlook.com>
2024-02-20 17:40:18 +1100

    Create folder multi-vms
    Create a new folder which contains scripts to automate creating multiple VMs with private ips only

---
+++ infra/multi-vms/variables.tf
@@ -0,0 +1,41 @@
+variable "location" {
+  type        = string
+  description = "The Azure region where all resources in this example should be created."
+  default     = "australiaeast"
+}
+
+variable "rg_name" {
+  type        = string
+  description = "The resource group name"
+  default     = "web-app-dev-rg"
+}
+
+variable "plan_tier" {
+  type        = string
+  description = "The tier of app service plan to create"
+  default     = "Basic"
+}
+
+variable "plan_sku" {
+  type        = string
+  description = "The sku of app service plan to create"
+  default     = "B1"
+}
+
+variable "environment" {
+  type        = string
+  description = "Name of the deployment environment"
+  default     = "dev"
+}
+
+variable "vm_pwd" {
+  type        = string
+  description = "VM admin password"
+  default     = "secret#44#20"
+}
+
+variable "vm_count" {
+  type        = number
+  description = "The number of VMs to create"
+  default = 3
+}
